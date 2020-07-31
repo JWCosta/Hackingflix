6bcbe5d6c2215e65bf37065c0c984c891730b4c1
JWCosta <jonathanwillian80@hotmail.com>
2020-07-31 02:18:22 -0300

    Update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { FooterBase } from './styles';
+import React from "react";
+import { FooterBase } from "./styles";
 
 function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img
+          src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg"
+          alt="Logo Alura"
+        />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Criado por <a href="https://github.com/JWCosta">Jonathan Costa</a>{" "}
+        durante a Imersão React da Alura.
       </p>
     </FooterBase>
   );
