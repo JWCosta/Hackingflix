83aafaa888a8adeac918170c7faa0c1f1da3bd5e
JWCosta <jonathanwillian80@hotmail.com>
2020-08-04 03:25:39 -0300

    Update db.json

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,15 +10,15 @@ function Home() {
     <div style={{ background: "#141414" }}>
       <Menu />
       <BannerMain
-        videoTitle={dadosIniciais.categorias[2].videos[0].titulo}
-        url={dadosIniciais.categorias[2].videos[0].url}
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={
           "O Que Linux? Um sistema operacional concorrente de Windows e macOS? O que é Kernel? Onde Linux é utilizado? Que empresas utilizam a ferramenta? É uma tecnologia open source e gratuita? Tudo isso e muito mais no vídeo sobre a definição de Linux para leigos e iniciantes."
         }
       />
-      <Carousel category={dadosIniciais.categorias[0]} />
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
       <Carousel category={dadosIniciais.categorias[1]} />
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
+      <Carousel category={dadosIniciais.categorias[2]} />
       <Carousel category={dadosIniciais.categorias[3]} />
       <Carousel category={dadosIniciais.categorias[4]} />
       <Carousel category={dadosIniciais.categorias[5]} />
